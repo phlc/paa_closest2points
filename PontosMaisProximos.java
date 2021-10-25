c2782c11f9e27b1fcd6a6431d76906f3cf4cd5c9
phlc <pedrocarvalho@outlook.com>
2021-10-25 09:21:59 -0300

    First Commit

---
+++ PontosMaisProximos.java
@@ -0,0 +1,13 @@
+/*
+Pucminas - Ciência da Computação - Coração Eucarístico
+PAA - Manhã
+Nome: Pedro Henrique Lima Carvalho
+Matricula: 651230
+
+Trabalho Pontos Mais Próximos:
+    Dado diversos pontos em um plano, encontrar os dois mais próximos.
+
+    Abordagem: Divisão e Conquista
+*/
+
+//Depencies 
