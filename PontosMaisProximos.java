a539841cd4ce05b17b16cf5285ee864ce3cc29f4
phlc <pedrocarvalho@outlook.com>
2021-10-25 10:19:05 -0300

    input read OK

--- PontosMaisProximos.java
+++ PontosMaisProximos.java
@@ -10,4 +10,50 @@ Trabalho Pontos Mais Próximos:
     Abordagem: Divisão e Conquista
 */
 
-//Depencies 
+//Dependencias 
+import java.util.Scanner;
+
+
+/*
+Classe Principal - PontosMaisProximos
+*/
+public class PontosMaisProximos{
+
+
+    /*
+    main
+    */
+    public static void main(String[] args){
+        //Declaracoes
+        Scanner reader = new Scanner(System.in);
+        int pontos = 0;
+
+        //Ler número de pontos
+        pontos = reader.nextInt();
+
+        //Repetir até entrada == 0
+        while(pontos!=0){
+            //Declaracoes
+            double[] posX = new double[pontos];
+            double[] posY = new double[pontos];
+
+            //Ler pontos
+            for(int i=0; i<pontos; i++){
+                posX[i] = reader.nextDouble();
+                posY[i] = reader.nextDouble();
+            }
+
+            System.out.println(pontos);
+            for(int i=0; i<pontos; i++){
+                System.out.println(posX[i]+" "+posY[i]);
+            }
+
+            //Ler nova entrada
+            pontos = reader.nextInt();
+        }
+
+        //Encerramento
+        reader.close();
+
+    }  
+}
