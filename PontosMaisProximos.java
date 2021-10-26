b1c5409a75075b6bf3243218e080d44bb1a5a7c1
phlc <pedrocarvalho@outlook.com>
2021-10-26 14:40:17 -0300

    Algorithm O(n^2) OK

--- PontosMaisProximos.java
+++ PontosMaisProximos.java
@@ -12,6 +12,49 @@ Trabalho Pontos Mais Próximos:
 
 //Dependencias 
 import java.util.Scanner;
+import java.lang.Math;
+
+/*
+Pontos - Classe que armazena as coordenadas de um ponto e sua posição nas ordenações
+         tanto pelo eixo X, quanto pelo eixo Y
+*/
+class Ponto{
+    //atributos
+    final private double x;
+    final private double y;
+    private int ordX;
+    private int ordY;
+
+
+    //Construtor
+    public Ponto(double x, double y){
+        this.x = x;
+        this.y = y;
+        this.ordX = this.ordY = -1;
+    }
+
+    //Sets & Gets
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public int getOrdX(){
+        return ordX;
+    }
+    public int getOrdY(){
+        return ordY;
+    }
+    public void setOrdX(int ordX){
+        this.ordX = ordX;
+    }
+    public void setOrdY(int ordY){
+        this.ordY = ordY;
+    }
+}
+
+
 
 
 /*
@@ -19,6 +62,45 @@ Classe Principal - PontosMaisProximos
 */
 public class PontosMaisProximos{
 
+    /*
+    distancia - calcula a distancia entre dois pontos
+    @param Ponto a, Ponto b
+    @return double distancia
+    */
+    private static double distancia(Ponto a, Ponto b){
+        double distancia = -1;
+        double difX = a.getX() - b.getX();
+        double difY = a.getY() - b.getY();
+        distancia = Math.sqrt(Math.pow(difX, 2) + Math.pow(difY, 2));
+        return distancia;
+    }
+
+    /*
+    algoritmoN2 - Soluciona o problema por força bruta em O(n^2)
+    @param Ponto[] - lista de pontos no plano
+    @return resposta[] - para de pontos mais próximos
+    */
+    public static Ponto[] algoritmoN2(Ponto[] lista){
+        //Declaracoes
+        Ponto[] resposta = new Ponto[2];
+        double menorDistancia = Double.POSITIVE_INFINITY;
+
+        //calcular todas as distancias
+        for(int i=0; i<lista.length; i++){
+            for(int j=i+1; j<lista.length; j++){
+                double dist = distancia(lista[i], lista[j]);
+
+                if(dist < menorDistancia){
+                    menorDistancia = dist;
+                    resposta[0] = lista[i];
+                    resposta[1] = lista[j];
+                }
+            }
+        }
+
+        //return
+        return resposta;
+    }
 
     /*
     main
@@ -26,30 +108,31 @@ public class PontosMaisProximos{
     public static void main(String[] args){
         //Declaracoes
         Scanner reader = new Scanner(System.in);
-        int pontos = 0;
+        int quantidade = 0;
 
         //Ler número de pontos
-        pontos = reader.nextInt();
+        quantidade = reader.nextInt();
 
         //Repetir até entrada == 0
-        while(pontos!=0){
+        while(quantidade!=0){
             //Declaracoes
-            double[] posX = new double[pontos];
-            double[] posY = new double[pontos];
+            Ponto[] lista = new Ponto[quantidade]; //lista de pontos
 
             //Ler pontos
-            for(int i=0; i<pontos; i++){
-                posX[i] = reader.nextDouble();
-                posY[i] = reader.nextDouble();
+            for(int i=0; i<quantidade; i++){
+                double x = reader.nextDouble();
+                double y = reader.nextDouble();
+                
+                //criar e inserir ponto na lista
+                lista[i] =  new Ponto(x, y);
             }
 
-            System.out.println(pontos);
-            for(int i=0; i<pontos; i++){
-                System.out.println(posX[i]+" "+posY[i]);
-            }
+            Ponto[] resposta = algoritmoN2(lista);
+            System.out.println("P1("+resposta[0].getX()+","+resposta[0].getY()+") P2("+resposta[1].getX()+","+resposta[1].getY()+")");
+
 
             //Ler nova entrada
-            pontos = reader.nextInt();
+            quantidade = reader.nextInt();
         }
 
         //Encerramento
